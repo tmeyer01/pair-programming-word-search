ff14ac5a5de410ccd3eca134e318ade36a64064d
thomas <tmeyerwebdev@gmail.com>
2021-09-23 00:21:29 +0000

    Inital commit for word search paird programming excersize with Sanam

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -6,24 +6,33 @@ const wordSearch = require('../wordsearch.js')
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
-      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['A', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['K', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      // ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      // ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      // ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      // ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'FRANK')
 
     assert.isFalse(result);
   });
+  it("should return false if empty array given", function() {
+    const result = wordSearch([
+      []
+      
+    ], '[]')
+
+    assert.isFalse(result);
+  });
+
 
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['S', 'E', 'i', 'N', 'F', 'E', 'L', 'D'],
       ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
       ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
@@ -35,4 +44,20 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['O', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['K', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['Y', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      
+    ], 'OKAY')
+
+    assert.isTrue(result);
+  });
+
+
+
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,56 @@
+// This paired project was a collab with @tmeyer01 
+
+
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
+    if(letters.length === 0){
+        return false;
+    }
+    
+    
+    const horizontalJoin = letters.map(ls => ls.join('').toUpperCase())
+   
+    //console.log(horizontalJoin);
+    
+    for (let l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    let vertWord = []
+
+    for(let col = 0; col < letters[0].length; col++){
+        let newWordArray = [];
+        for (let row = 0; row < letters.length; row++){
+            newWordArray.push(letters[row][col])
+        }
+        vertWord.push(newWordArray)
+    }
+
+    let verticalWordJoin = vertWord.map(i => i.join('').toUpperCase())
+    //console.log(verticalWordJoin);
+
+    for (let w of verticalWordJoin) {
+        if (w.includes(word)) return true
+    }
+
+    return false;
+
 }
 
+// wordSearch([
+//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+//   ]);
+
+
+
+
 module.exports = wordSearch
+
+//sanam1060
